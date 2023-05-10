86a8334b8357d1512a621fe1dcf8558782d49041
bartheart <bemnetmerkebu17@gmail.com>
2023-05-10 09:05:16 -0500

    intitial commit

---
+++ templates/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Professional Blog</title>
+</head>
+
+<body>
+    <div class="nav-container">
+        <div>
+            <nav>
+                <div>
+                    <p>"~% shell"</p>
+                </div>
+                <ul>
+                    <li>HOME</li>
+                    <li>PROJECTS</li>
+                    <li>EXPERIENCE</li>
+                    <li>ABOUT ME</li>
+                    <li>ASPIRATIONS</li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+
